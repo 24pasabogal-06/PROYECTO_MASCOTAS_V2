18c8ee68841edb788837f4a98be6593604a10b09
24pasabogal-06 <pasabogalg@udistrital.edu.co>
2025-06-25 17:21:35 -0500

    se hacen las clases junto al gui

---
+++ src/Interfaz.java
@@ -0,0 +1,15 @@
+package GUI;
+
+import models.Dueño;
+import models.Mascota;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+
+public class Interfaz {
+
+}
