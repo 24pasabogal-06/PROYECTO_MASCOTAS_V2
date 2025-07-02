38691d49a0b127a784cb23e7a81b6cd09b2cb094
24pasabogal-06 <pasabogalg@udistrital.edu.co>
2025-07-02 17:35:35 -0500

    toco parcharse

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
       // Establecer apariencia del sistema (opcional)
       try {
          UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-      } catch (Exception ignored) {}
+      } catch (Exception ignored) {
+         // Se ignora cualquier excepción para no detener la aplicación
+      }
 
-      // Iniciar desde la interfaz de login
+      // Iniciar desde la interfaz de Login
       javax.swing.SwingUtilities.invokeLater(() -> {
          Login login = new Login();
          login.setVisible(true);
